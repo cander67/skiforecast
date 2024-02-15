bd781d05d8bd596fe52788af01a0a8a24335dcf6
Cyrus Anderson <anderson.cyrus@gmail.com>
2024-02-15 09:18:44 -0800

    update import statements, add path for reading and writing files

--- test/test_forecast_proc.py
+++ test/test_forecast_proc.py
@@ -1,3 +1,5 @@
+### Run in terminal: python3 -m test.test_forecast_proc
+
 import os
 import json
 from datetime import datetime
@@ -5,7 +7,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 #from azure.identity import DefaultAzureCredential
 #from azure.storage.blob import BlobServiceClient, ContainerClient, ContentSettings
-import src.utils as utils
+from src import utils as utils
 #import src.get_endpoints as get_endpoints
 #import src.get_forecasts as get_forecasts
 #import src.proc_forecasts as proc_forecasts
@@ -20,6 +22,8 @@ locations = json.loads(os.getenv("LOCATIONS"))
 time_periods = json.loads(os.getenv("TIME_PERIODS"))
 properties = json.loads(os.getenv("PROPERTIES"))
 
+path = 'test/'
+
 files = {
     "Mt. Baker": "Mt. Baker_gridData.json",
     "Loup Loup": "Loup Loup_gridData.json",
@@ -31,7 +35,7 @@ files = {
 }
 
 #for location in files.keys:
-with open(files["Loup Loup"], 'r') as f:
+with open(f'{path}{files["Loup Loup"]}', 'r') as f:
     data = json.load(f)
 f.close()
 
@@ -45,7 +49,7 @@ except Exception as e:
     print(f'Error parsing forecast, {"Loup Loup"}: {e}')
 
 # Print parsed forecast data as json
-with open('Loup Loup_parsed.json', 'w') as f:
+with open(f'{path}Loup Loup_parsed.json', 'w') as f:
     print(json.dumps(parsed, sort_keys=False, indent=4), file = f)
 f.close()
 
@@ -56,6 +60,6 @@ except Exception as e:
     print(f'Error calculating table data, {"Loup Loup"}: {e}')
 
 # Print table data as json
-with open('Loup Loup_table_data.json', 'w') as f:
+with open(f'{path}Loup Loup_table_data.json', 'w') as f:
     print(json.dumps(table_data, sort_keys=False, indent=4), file = f)
 f.close()
