2e6522a10e4be9c6d3697e138ba83d11512a5c0a
Cyrus Anderson <anderson.cyrus@gmail.com>
2024-02-22 13:05:51 -0800

    add parsing, table_data, and row preparation

--- test/test_forecast_proc.py
+++ test/test_forecast_proc.py
@@ -10,7 +10,7 @@ now = datetime.now(pytz.UTC)
 local_time = now.astimezone(pytz.timezone('US/Pacific'))
 
 # Define parameters
-locations = LOCATIONS = {
+locations = {
     "Mt. Baker": [[48.8618, -121.6789], [3500, 5000], ["https://www.mtbaker.us/snow-report/"]], 
     "Loup Loup": [[48.3940, -119.9111], [4020, 5260], ["https://skitheloup.org/mountain/conditions-weather/"]], 
     "Stevens Pass": [[47.7439, -121.0908], [4061, 5845], ["https://www.stevenspass.com/the-mountain/mountain-conditions/weather-report.aspx"]], 
@@ -20,7 +20,7 @@ locations = LOCATIONS = {
     "Crystal Mountain": [[46.4350, -121.4751], [4600, 7002], ["https://www.crystalmountainresort.com/the-mountain/mountain-report-and-webcams#/"]]
     }
 
-time_periods = TIME_PERIODS = {
+time_periods = {
     "day0": ["24h", "am", "pm", "overnight"], 
     "day1": ["24h", "am", "pm", "overnight"], 
     "day2": ["24h", "am", "pm", "overnight"], 
@@ -43,52 +43,59 @@ properties = {
     "snowLevel": {"units": "ft", "calculations": ["min", "max"]}
     }
 
-
 path = 'test/'
 
 files = {
     "Mt. Baker": "Mt. Baker_gridData.json",
-    "Loup Loup": "Loup Loup_gridData.json",
-    "Stevens Pass": "Stevens Pass_gridData.json",
-    "Snoqualmie Pass": "Snoqualmie Pass_gridData.json",
-    "Mission Ridge": "Mission Ridge_gridData.json",
-    "White Pass": "White Pass_gridData.json",
-    "Crystal Mountain": "Crystal Mountain_gridData.json"
+    #"Loup Loup": "Loup Loup_gridData.json",
+    #"Stevens Pass": "Stevens Pass_gridData.json",
+    #"Snoqualmie Pass": "Snoqualmie Pass_gridData.json",
+    #"Mission Ridge": "Mission Ridge_gridData.json",
+    #"White Pass": "White Pass_gridData.json",
+    #"Crystal Mountain": "Crystal Mountain_gridData.json"
 }
 
-#for location in files.keys:
-with open(f'{path}{files["Loup Loup"]}', 'r') as f:
-    data = json.load(f)
-f.close()
+print(files.keys())
 
-# Instantiate TableData object
-setup = utils.TableData(now, "Loup Loup", time_periods, properties)
+for file in files.keys():
+    with open(f'{path}{files[file]}', 'r') as f:
+        data = json.load(f)
+        print(f'LOCATION: {file}\n')
+    f.close()
 
-# Parse forecast data
-try:
-    parsed = setup.parse_forecast(data)
-except Exception as e:
-    print(f'Error parsing forecast, {"Loup Loup"}: {e}')
+    # Instantiate TableData object
+    setup = utils.TableData(now, file, time_periods, properties)
+    print(f'TABLE DATA: {setup}\n')
 
-# Print parsed forecast data as json
-with open(f'{path}Loup Loup_parsed.json', 'w') as f:
-    print(json.dumps(parsed, sort_keys=False, indent=4), file = f)
-f.close()
+    # Parse forecast data
+    try:
+        parsed = setup.parse_forecast(data)
+        print(f'PARSED\n')
+    except Exception as e:
+        print(f'ERROR PARSING FORECAST, {file}: {e}\n')
 
-# Calculate table data
-try:
-    table_data = setup.calculate_table_data(parsed)
-except Exception as e:
-    print(f'Error calculating table data, {"Loup Loup"}: {e}')
+    # Save parsed forecast data as json
+    with open(f"{path}{file}_parsed.json", 'w') as f:
+        print(json.dumps(parsed, sort_keys=False, indent=4), file = f)
+        print(f'PARSED DATA SAVED AS: {path}{file}_parsed.json\n')
+    f.close()
 
-# Print table data as json
-with open(f'{path}Loup Loup_table_data.json', 'w') as f:
-    print(json.dumps(table_data, sort_keys=False, indent=4), file = f)
-f.close()
+    # Calculate table data
+    try:
+        table_data = setup.calculate_table_data(parsed)
+        print(f'TABLE DATA CALCULATED\n')
+    except Exception as e:
+        print(f'ERROR CALCULATING TABLE DATA, {file}: {e}\n')
 
-# Create table row
-try:
-    row = setup.create_row(table_data)
-    #print(f'ROW: {row}')
-except Exception as e:
-    print(f'Error creating table row: {e}')
+    # Save table data as json
+    with open(f'{path}{file}_table_data.json', 'w') as f:
+        print(json.dumps(table_data, sort_keys=False, indent=4), file = f)
+        print(f'TABLE DATA SAVED AS: {path}{file}_table_data.json\n')
+    f.close()
+
+    # Create table row
+    try:
+        row = setup.create_row(table_data)
+        print(f'ROW: {row}')
+    except Exception as e:
+        print(f'ERROR CREATING TABLE ROW, {file}: {e}')
