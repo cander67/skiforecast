00a0ec997ae7831b45bfc9d83e483355d974b502
Cyrus Anderson <anderson.cyrus@gmail.com>
2024-02-22 14:29:49 -0800

    add html file creation to test routine

--- test/test_forecast_proc.py
+++ test/test_forecast_proc.py
@@ -4,10 +4,13 @@ import os
 import json
 from datetime import datetime
 import pytz
+import bs4 as BeautifulSoup
 import utils as utils
 
-now = datetime.now(pytz.UTC)
+#now = datetime.now(pytz.UTC)
+now = datetime(2024, 2, 22, 13, 8, 0, 0, pytz.UTC)
 local_time = now.astimezone(pytz.timezone('US/Pacific'))
+
 
 # Define parameters
 locations = {
@@ -47,15 +50,16 @@ path = 'test/'
 
 files = {
     "Mt. Baker": "Mt. Baker_gridData.json",
-    #"Loup Loup": "Loup Loup_gridData.json",
-    #"Stevens Pass": "Stevens Pass_gridData.json",
-    #"Snoqualmie Pass": "Snoqualmie Pass_gridData.json",
-    #"Mission Ridge": "Mission Ridge_gridData.json",
-    #"White Pass": "White Pass_gridData.json",
-    #"Crystal Mountain": "Crystal Mountain_gridData.json"
+    "Loup Loup": "Loup Loup_gridData.json",
+    "Stevens Pass": "Stevens Pass_gridData.json",
+    "Snoqualmie Pass": "Snoqualmie Pass_gridData.json",
+    "Mission Ridge": "Mission Ridge_gridData.json",
+    "White Pass": "White Pass_gridData.json",
+    "Crystal Mountain": "Crystal Mountain_gridData.json"
 }
 
-print(files.keys())
+table = utils.Table()
+table.create_columns(local_time)
 
 for file in files.keys():
     with open(f'{path}{files[file]}', 'r') as f:
@@ -96,6 +100,101 @@ for file in files.keys():
     # Create table row
     try:
         row = setup.create_row(table_data)
-        print(f'ROW: {row}')
+        # Append row to table
+        #table.append_row(row)
+        #print(f'ROW:\n{json.dumps(row, sort_keys=False, indent=4)}\n')
     except Exception as e:
         print(f'ERROR CREATING TABLE ROW, {file}: {e}')
+
+    table.append_row(row)
+
+#print(f'TABLE: {table.get_table()}\n')
+t = table.get_table()
+
+#Assign columns and rows
+columns = t['columns']
+rows = t['rows']
+
+#print(f'COLUMNS:\n{columns}\n')
+#print(f'ROWS:\n{rows}\n')
+
+# Get dates
+day0 = datetime.strptime(columns[1][1], '%Y-%m-%d')
+day6 = datetime.strptime(columns[7][1], '%Y-%m-%d')
+start = day0.strftime('%B %d %Y')
+end = day6.strftime('%B %d %Y')
+
+# Create HTML output
+html = "<!DOCTYPE html>\n"
+html += "<html lang='en'>\n<head>\n"
+html += '<link rel="stylesheet" type="text/css" href="site.css">\n'
+html += '<meta charset="UTF-8">\n'
+html += '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
+html += '<title>Ski Weather Outlook</title>\n'
+html += "</head>\n<body>\n"
+html += '<div class="header">'
+html += '<a href="https://www.digitalglissade.com/home">Digital Glissade</a>\n<a href="https://www.digitalglissade.com/about">About</a>\n<a href="https://www.digitalglissade.com/services">Services</a>\n<a href="https://www.digitalglissade.com/blog">Blog</a>\n<a href="https://www.digitalglissade.com/projects">Projects</a>\n<a href="https://www.digitalglissade.com/contact">Contact</a>\n'
+html += "</div>\n"
+html += f"<h1>Ski Weather Outlook</h1>\n"
+html += f"<h2>{start} - {end}</h2>\n"
+html += "<table id='weather-data'>\n"
+
+# Write table header
+html += "<thead>\n<tr>"
+for column in columns:
+    tooltip_text = str(column[1])  # Tooltip text
+    html += f"<th title='{tooltip_text}'>{column[0]}</th>"
+html += "</tr>\n</thead>\n"
+
+# Write table rows
+html += "<tbody>\n"
+for row in rows:
+    cell_count = 0
+    html += "<tr>"
+    for cell in row:
+        if cell_count == 0:
+            cell_content = str(cell[0])
+            words = cell_content.split('\n')
+            words[0] = f'<a href="{cell[3]}">{words[0]}</a>'
+            cell_content = '<br>'.join(words)
+            tooltip_text = str(cell[1])  # Tooltip text
+            html += f'<td class="cell-style-{cell[2]}" title="{tooltip_text}">{cell_content}</td>'
+            cell_count += 1
+        elif cell_count > 0:
+            cell_content = str(cell[0]).replace('\n', '<br>')
+            tooltip_text = str(cell[1])  # Tooltip text
+            html += f'<td class="cell-style-{cell[2]}" title="{tooltip_text}">{cell_content}</td>'
+    html += "</tr>\n"
+html += "</tbody>\n"
+
+# End the HTML output
+html += "</table>"
+html += f"<h3>Updated: {local_time.strftime('%Y-%m-%d %H:%M')} (PT)</h3>\n"
+html += '<section id="notes">\n<h3>NOTES</h3>\n'
+html += "<dl>\n<dt>- Data compiled from <a href='https://www.noaa.gov/'>NOAA</a> and scored according to subjective criteria for what makes a great ski day</dt>\n"
+html += '<dt>- Hover over table cells for more data</dt>\n'
+html += "<dt>- STATUS: <span class='color-1'>RED</span> = Don't Bother | <span class='color-2'>YELLOW</span> = Maybe | <span class='color-3'>GREEN</span> = Shred on!</dt>\n"
+html += '<dt>- MIX: Rain/Snow mixture; forecast snowfall amount reported</dt>\n'
+html += '<dt>- Trace = < 0.1in forecast precip</dt>\n'
+html += '<dt>- SLVL: Snow level; min & max for 24 hrs (6am to 6am) starting on the forecast date</dt>\n'
+html += '<dt>- AM|PM|ON: avg temp, morning = 6am - 12pm | afternoon = 12pm - 6pm | overnight = 6pm - 6am</dt>\n'
+html += '<dt>- MIN|MAX: min & max temp for 24 hrs (6am to 6am) starting on the forecast date</dt>\n'
+html += '<dt>- Questions? Comments? Suggestions? See email link below.</dt>\n'
+html += '</dl>\n'
+html += '<footer>\n'
+html += '<div class="footer-content">\n'
+html += '<p><a href="mailto:info@digitalglissade.com">info@digitalglissade.com</a></p>\n'
+html += '<p>&copy; 2024 Digital Glissade. All rights reserved.</p>\n'
+html += '<a href="https://www.digitalglissade.com/terms-of-use">Terms of Use</a> | <a href="https://www.digitalglissade.com/privacy-policy">Privacy Policy</a> | <a href="https://www.digitalglissade.com/cookie-policy">Cookies Policy</a>\n'
+html += '</div>\n</footer>\n'
+html += "</body>\n</html>"
+
+# Prepare html file for blob write
+soup = BeautifulSoup.BeautifulSoup(html, 'html.parser')
+pretty_html = soup.prettify()
+html_file = 'ski.html'
+
+# Write html file
+with open(f'{path}{html_file}', 'w') as f:
+    print(pretty_html, file = f)
+    print(f'HTML FILE SAVED AS: {path}{html_file}\n')
